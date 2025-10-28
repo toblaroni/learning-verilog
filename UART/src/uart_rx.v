4bb7e7e036595e5477587e786a790418e65006f5
Toby Hutchinson <tobyhutchinson@gmail.com>
2025-10-28 19:18:15 +0000

    Quick style from the youth hostel

--- UART/src/uart_rx.v
+++ UART/src/uart_rx.v
@@ -15,15 +15,13 @@ module uart_rx #
 );
 
 localparam CLKS_PER_BIT = CLOCK_FREQ / BAUD_RATE;
-// We want to sample at around x8 or x16 the baud rate...
-localparam SAMPLES_PER_BIT = CLKS_PER_BIT / 8;
 
 reg [31:0] bit_count;
 reg [2:0] bit_index;
-reg [31:0] sample_count;    // We need some way of keeping track of which bit we're receiving
+reg [2:0] state;
 
-wire baud_tick   = (bit_count == CLKS_PER_BIT-1);
-wire sample_tick = (sample_count == SAMPLES_PER_BIT-1);
+wire baud_tick = (bit_count == CLKS_PER_BIT-1);
+wire sample_tick = (bit_count == (CLKS_PER_BIT-1)/2);
 
 // States
 localparam IDLE  = 3'b00;
@@ -36,7 +34,6 @@ always @(posedge clk) begin
         IDLE: begin
             bit_index <= 3'b0;
             bit_count <= 32'b0;
-            sample_count <= 32'b0;
 
             if (data_in == 1'b0) begin
                 state <= START; 
@@ -44,21 +41,32 @@ always @(posedge clk) begin
         end
 
         START: begin
-            // Read in the start bit
-            if (baud_tick) begin
-                
+            // Make sure that it's actually the start bit and not noise
+            if (sample_tick && data_in == 1'b1) begin
+                state <= IDLE;  // If noise go back
             end
+            if (baud_tick) state <= DATA;   // Move onto the next stage
         end
 
         DATA: begin
+            if (sample_tick)
+                rx[bit_index] <= data_in;
+            if (baud_tick) begin
+                if (bit_index == 3'b111)
+                    state <= STOP;
+                else
+                    bit_index <= bit_index + 1;
+            end
         end
 
         STOP: begin
+            if (sample_tick && data_in == 1'b1)
+                state <= IDLE;
         end
+        default: state <= IDLE;
     endcase
 
     bit_count <= (baud_tick) ? 0 : bit_count + 1;
-    sample_count <= (sample_tick) ? 0 : bit_count + 1;
 end
 
 endmodule
